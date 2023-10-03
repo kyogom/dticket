7c88c9f2185cd4f9c99e95a17480bedc8677e262
kyogom <npdijeb1@gmail.com>
2023-10-03 17:47:29 +0900

    test

--- backend/src/interactions/interactions.module.ts
+++ backend/src/interactions/interactions.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { InteractionsController } from './interactions.controller';
 import { InteractionsService } from './interactions.service';
 
+
 @Module({
   controllers: [InteractionsController],
   providers: [InteractionsService],
